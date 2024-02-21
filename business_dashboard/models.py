78bb6d842888aebb6ff1b80783699565a222c939
Pratik Choudhari <pratik.choudhari89@gmail.com>
2024-02-21 17:03:24 -0500

    [UPDATE] add business model

--- business_dashboard/models.py
+++ business_dashboard/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Business(User):
+    registered_enum = [
+        (0, 'Not Registered'),
+        (1, 'Registered'),
+        (2, 'Registration in process')]
+
+    business_name = models.CharField(max_length=200, unique=True, null=False)
+    phone_number = models.CharField(max_length=10, unique=True, null=False)
+    is_registered = models.IntegerField(choices=registered_enum, null=False, default=0)
+
+    class Meta:
+        verbose_name = "Business"
+
+    def __str__(self):
+        return f"Business: {self.first_name} {self.last_name} {self.business_name}"
