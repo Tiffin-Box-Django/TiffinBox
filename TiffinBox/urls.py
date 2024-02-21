43ea5b4cb41aa9146c1d113d5b6923a26e29c83e
Pratik Choudhari <pratik.choudhari89@gmail.com>
2024-02-21 16:14:16 -0500

    [REFACTOR] rename admin app to business

--- TiffinBox/urls.py
+++ TiffinBox/urls.py
@@ -5,5 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include("user_dashboard.urls")),
-    path('business/', include("admin_dashboard.urls"))
+    path('business/', include("business_dashboard.urls"))
 ]
