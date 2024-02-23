bd5a397d7363282c5f11880318ba0d43554a610f
samreethawilliams <samreethawilliams@gmail.com>
2024-02-23 17:11:54 -0500

    [UPDATE] modified search bar

--- business_dashboard/views.py
+++ business_dashboard/views.py
@@ -4,3 +4,5 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'base.html', {})
 
+def tiffin(request):
+    return render(request, template_name='tiffin_list.html', context={})
