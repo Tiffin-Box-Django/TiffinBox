d43f00a0e659753d484f65a305a86ce1a9653037
Pratik Choudhari <pratik.choudhari89@gmail.com>
2024-02-23 13:53:16 -0500

    [UPDATE] add User Model

--- user_dashboard/models.py
+++ user_dashboard/models.py
@@ -1,4 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class TBUser(User):
+    CLIENT_TYPE = [
+        (0, 'User'),
+        (1, 'Business')]
+
+    profile_picture = models.TextField()
+    phone_number = models.CharField(max_length=10)
+    client_type = models.IntegerField(choices=CLIENT_TYPE, default=0)
+    is_registered = models.BooleanField(default=False)
+    shipping_address = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name = "TBUser"
 
 
 class TiffinItemList(models.Model):
@@ -11,10 +27,10 @@ class TiffinItemList(models.Model):
 
 class Schedule(models.Model):
     choices = [
-        (1,'One Day Trial'),
-        (2,'Daily'),
-        (3,'Weekly'),
-        (4,'Monthly')
+        (1, 'One Day Trial'),
+        (2, 'Daily'),
+        (3, 'Weekly'),
+        (4, 'Monthly')
     ]
     frequency = models.IntegerField(choices=choices)
 
