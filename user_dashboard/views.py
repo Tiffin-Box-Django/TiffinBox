0d143ca3f2cdf650c1462ab6e4dda62b7a9003a4
Dhvani Sheth <dhvanisheth25@gmail.com>
2024-02-26 15:20:13 -0500

    design for explore page with dummy data and basic styling

--- user_dashboard/views.py
+++ user_dashboard/views.py
@@ -4,5 +4,18 @@ from django.shortcuts import render
 def explore(request):
     if request.method != 'GET':
         return {"error": "Method Not Allowed"}
-    return render(request, 'user_dashboard/explore.html', {})
-
+    tiffins = [
+        {
+            "name": "Tiffin 1",
+            "photo": "img/breakfast.jpg",
+            "business": "Business A",
+            "rating": [1, 2, 3, 4]
+        },
+        {
+            "name": "Tiffin 2",
+            "photo": "img/lunch.jpeg",
+            "business": "Business B",
+            "rating": [1, 2, 3]
+        }
+    ]
+    return render(request, 'user_dashboard/explore.html', {'tiffins': tiffins})
