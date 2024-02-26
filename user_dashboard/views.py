9995b93091a94256eb9ce5e3cb1633fa184989d2
Pratik Choudhari <pratik.choudhari89@gmail.com>
2024-02-26 17:30:45 -0500

    [UPDATE] show tiffins on explore page by querying Tiffin model

--- user_dashboard/views.py
+++ user_dashboard/views.py
@@ -1,21 +1,19 @@
 from django.shortcuts import render
+from django.db.models import F
+
+from .models import Tiffin
 
 
 def explore(request):
     if request.method != 'GET':
         return {"error": "Method Not Allowed"}
-    tiffins = [
-        {
-            "name": "Tiffin 1",
-            "photo": "img/breakfast.jpg",
-            "business": "Business A",
-            "rating": [1, 2, 3, 4]
-        },
-        {
-            "name": "Tiffin 2",
-            "photo": "img/lunch.jpeg",
-            "business": "Business B",
-            "rating": [1, 2, 3]
-        }
-    ]
+    query_set = (Tiffin.objects.all()
+                 .values("tiffin_name", "image", "business_id__first_name", "business_id__last_name", "avg_rating")
+                 .annotate(name=F("tiffin_name"), photo=F("image"), business=F("business_id__first_name"),
+                           rating=F("avg_rating")))
+    tiffins = []
+    for tiffin in query_set:
+        tiffins.append({"name": tiffin["tiffin_name"], "photo": tiffin["image"],
+                        "business": tiffin["business_id__first_name"] + tiffin["business_id__last_name"],
+                        "rating": list(range(int(round(tiffin["avg_rating"]))))})
     return render(request, 'user_dashboard/explore.html', {'tiffins': tiffins})
