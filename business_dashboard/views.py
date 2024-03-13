e29b4df72c5065ef173c230a3a6eb9ef4bd44456
samreethawilliams <samreethawilliams@gmail.com>
2024-03-13 06:17:20 -0400

    added add_tiffin form

--- business_dashboard/views.py
+++ business_dashboard/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 
+from business_dashboard.forms import TiffinForm
+from user_dashboard.models import Tiffin
+
 
 def index(request):
     return render(request, 'base.html', {})
 
 def tiffin(request):
     return render(request, template_name='business_dashboard/tiffin_list.html', context={})
+
+def add_tiffin(request):
+    msg=''
+    if request.method == 'POST':
+        form = TiffinForm(request.POST)
+        if form.is_valid():
+            tiffin_item = form.save(commit=False)
+            tiffin_item.save()
+            msg='Tiffin added'
+    else:
+        form = TiffinForm()
+    return render(request, 'business_dashboard/add_tiffin.html', context={'form': form, 'msg': msg})
+
+
