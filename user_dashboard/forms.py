27dac2a20f32d75d3c776c62f5b48b378f09edb1
Pratik Choudhari <pratik.choudhari89@gmail.com>
2024-03-15 17:53:52 -0400

    [FIX] use model forms for explore page filter

--- user_dashboard/forms.py
+++ user_dashboard/forms.py
@@ -1,4 +1,9 @@
+from math import ceil
+
 from django import forms
+from django.db.models import Max, Min
+
+from user_dashboard.models import Tiffin
 
 
 class ExploreSearchForm(forms.Form):
@@ -6,26 +11,57 @@ class ExploreSearchForm(forms.Form):
 
 
 RATING_CHOICES = [
-    ('5', '5'),
-    ('4', '4'),
-    ('3', '3'),
-    ('2', '2'),
-    ('1', '1'),
+    ('5', '⭐⭐⭐⭐⭐'),
+    ('4', '⭐⭐⭐⭐'),
+    ('3', '⭐⭐⭐'),
+    ('2', '⭐⭐'),
+    ('1', '⭐'),
 ]
 
-MEAL_TYPE = [
-    ('Lunch', 'Lunch'),
-    ('Dinner', 'Dinner')
-]
+MEAL_TYPE = [(0, 'VEG'), (1, 'NON-VEG'), (2, 'VEGAN')]
 
 
-class FilterForm(forms.Form, forms.RegexField):
-    rating = forms.ChoiceField(choices=RATING_CHOICES, widget=forms.CheckboxSelectMultiple, required=False)
-    meal_type = forms.ChoiceField(choices=MEAL_TYPE, widget=forms.CheckboxSelectMultiple, required=False)
-    calorie_range = forms.IntegerField(label='Calorie Range', required=False)
-    price_range = forms.IntegerField(label='Price Range', required=False)
+# class FilterForm(forms.Form):
+#     rating = forms.ChoiceField(choices=RATING_CHOICES, widget=forms.CheckboxSelectMultiple, required=False)
+#     meal_type = forms.ChoiceField(choices=MEAL_TYPE, widget=forms.CheckboxSelectMultiple, required=False)
+#     calorie_range = forms.IntegerField(label='Calorie Range', required=False, min_value=0, max_value=5,
+#                                        # attrs={'class': 'form-control'},
+#                                        widget=forms.NumberInput(attrs={'class': 'form-group numeric-slider',
+#                                                                        "type": "range",
+#                                                                        "data-range-min": "0",
+#                                                                        "data-range-max": "5"}))
+#     price_range = forms.IntegerField(label='Price Range', required=False)
 
-    widgets = {
-        'calorie_range': forms.RangeInput(attrs={'type': 'range', 'min': 0, 'max': 1000}),
-        'price_range': forms.RangeInput(attrs={'type': 'range', 'min': 0, 'max': 1000}),
-    }
+def create_price_range():
+    min_ = float(Tiffin.objects.all().values_list('price', flat=True).annotate(Min('price')).order_by('price')[0])
+    max_ = float(Tiffin.objects.all().values_list('price', flat=True).annotate(Max('price')).order_by('-price')[0])
+    min_ = int(min_ - (min_ % 10))
+    max_ = int(max_ - (max_ % 10)) + 10
+    ranges = list(range(min_, max_, 10))
+    ranges = list(zip(ranges, ranges[1:]))
+    ranges = [(f"${r[0]} - ${r[1]}", f"${r[0]} - ${r[1]}") for r in ranges]
+    return ranges
+
+
+def create_calorie_range():
+    min_ = float(Tiffin.objects.all().values_list('calories', flat=True)
+                 .annotate(Min('calories')).order_by('calories')[0])
+    max_ = float(Tiffin.objects.all().values_list('calories', flat=True)
+                 .annotate(Max('calories')).order_by('-calories')[0])
+    min_ = int(min_ - (min_ % 100))
+    max_ = int(max_ - (max_ % 100)) + 100
+    ranges = list(range(min_, max_, 100))
+    ranges = list(zip(ranges, ranges[1:]))
+    ranges = [(f"{r[0]} - {r[1]}", f"{r[0]} - {r[1]}") for r in ranges]
+    return ranges
+
+
+class FilterForm(forms.ModelForm):
+    class Meta:
+        model = Tiffin
+        exclude = ["image", "tiffin_name", "tiffin_description", "schedule_id", "business_id"]
+        widgets = {"meal_type": forms.CheckboxSelectMultiple(),
+                   "avg_rating": forms.CheckboxSelectMultiple(choices=RATING_CHOICES),
+                   "price": forms.RadioSelect(choices=create_price_range()),
+                   "calories": forms.RadioSelect(choices=create_calorie_range())}
+        labels = {"avg_rating": "Rating"}
--- user_dashboard/views.py
+++ user_dashboard/views.py
@@ -9,28 +9,20 @@ from .forms import ExploreSearchForm, FilterForm
 def explore(request):
     searchForm = ExploreSearchForm()
     filtersForm = FilterForm()
-    query_set = (Tiffin.objects.all()
-                 .values("tiffin_name", "image", "business_id__first_name", "business_id__last_name", "avg_rating",
-                         "price"))
+    query_set = Tiffin.objects.all().values("tiffin_name", "image", "business_id__first_name",
+                                            "business_id__last_name", "avg_rating", "price", "id")
     tiffins = []
-    if searchForm.is_valid() and 'query' in request.GET:
+    if 'query' in request.GET:
         search = request.GET.get("search")
         query_set = query_set.filter(tiffin_name__contains=search)
-    else:
-        query_set = query_set
 
-    print(filtersForm.is_valid())
-    print(filtersForm.changed_data)
-
-    if filtersForm.is_valid() and filtersForm.changed_data:
+    if filtersForm.changed_data and False:
         mealType = filtersForm.cleaned_data.get('meal_type')
         rating = filtersForm.cleaned_data.get('rating')
-        calorie_min = filtersForm.cleaned_data.get('calorie_range')
-        calorie_max = filtersForm.cleaned_data.get('calorie_range')
-        price_min = filtersForm.cleaned_data.get('price_range')
-        price_max = filtersForm.cleaned_data.get('price_range')
-        print(mealType, rating)
-        print(calorie_min, calorie_max, price_min, price_max)
+        # calorie_min = filtersForm.cleaned_data.get('calorie_range')
+        # calorie_max = filtersForm.cleaned_data.get('calorie_range')
+        # price_min = filtersForm.cleaned_data.get('price_range')
+        # price_max = filtersForm.cleaned_data.get('price_range')
 
         if mealType:
             query_set = query_set.filter(meal_type=mealType)
@@ -44,16 +36,14 @@ def explore(request):
         tiffins.append({"name": tiffin["tiffin_name"], "photo": tiffin["image"],
                         "business": tiffin["business_id__first_name"] + tiffin["business_id__last_name"],
                         "rating": list(range(int(round(tiffin["avg_rating"])))),
-                        "price": tiffin["price"]})
+                        "price": tiffin["price"], "id": tiffin["id"]})
 
-    meal_types = Tiffin.objects.values_list("meal_type", flat=True).distinct()
-    meal_types = [MEAL[i] for i in meal_types]
     paginator = Paginator(tiffins, 9)  # Show 9 dishes per page
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     return render(request, 'user_dashboard/explore.html',
                   {'searchForm': searchForm, 'filtersForm': filtersForm,
-                   'tiffins': page_obj, "meal_types": meal_types})
+                   'tiffins': page_obj, "filter_params": {}})
 
 
 def tiffindetails(request, tiffin_id):
