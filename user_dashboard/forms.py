3d0e7df8dc1ad911cce750c5ef6bc035c32d3e20
Dhvani Sheth <dhvanisheth25@gmail.com>
2024-03-17 13:02:21 -0400

    forms.py merge conflict resolved

--- user_dashboard/forms.py
+++ user_dashboard/forms.py
@@ -1,36 +1,3 @@
-from django import forms
-<<<<<<< HEAD
-
-
-class ExploreSearchForm(forms.Form):
-    search = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Search by business name or food item'}))
-
-
-RATING_CHOICES = [
-    ('5', '5'),
-    ('4', '4'),
-    ('3', '3'),
-    ('2', '2'),
-    ('1', '1'),
-]
-
-MEAL_TYPE = [
-    ('Lunch', 'Lunch'),
-    ('Dinner', 'Dinner')
-]
-
-
-class FilterForm(forms.Form, forms.RegexField):
-    rating = forms.ChoiceField(choices=RATING_CHOICES, widget=forms.CheckboxSelectMultiple, required=False)
-    meal_type = forms.ChoiceField(choices=MEAL_TYPE, widget=forms.CheckboxSelectMultiple, required=False)
-    calorie_range = forms.IntegerField(label='Calorie Range', required=False)
-    price_range = forms.IntegerField(label='Price Range', required=False)
-
-    widgets = {
-        'calorie_range': forms.RangeInput(attrs={'type': 'range', 'min': 0, 'max': 1000}),
-        'price_range': forms.RangeInput(attrs={'type': 'range', 'min': 0, 'max': 1000}),
-    }
-=======
 from django.db.models import Max, Min
 
 from user_dashboard.models import Tiffin
@@ -83,4 +50,3 @@ class FilterForm(forms.ModelForm):
                    "price": forms.RadioSelect(choices=create_price_range()),
                    "calories": forms.RadioSelect(choices=create_calorie_range())}
         labels = {"avg_rating": "Rating"}
->>>>>>> 90e0fb0a7ffdf76bad4852e6326f4f7f73948c91
