ea796dd8a08213fe299bdac9890529d57f12da76
Dhvani Sheth <dhvanisheth25@gmail.com>
2024-03-17 15:46:57 -0400

    fixed issues

--- user_dashboard/views.py
+++ user_dashboard/views.py
@@ -1,51 +1,49 @@
 from django.shortcuts import render
 
 from .models import Tiffin
-from enum_maps import MEAL
 from .forms import ExploreSearchForm, FilterForm
 
 
 def explore(request):
-    searchForm = ExploreSearchForm()
-    filtersForm = FilterForm()
-    query_set = (Tiffin.objects.all()
-                 .values("tiffin_name", "image", "business_id__first_name", "business_id__last_name", "avg_rating",
-                         "price"))
-    tiffins = []
-    if searchForm.is_valid() and 'query' in request.GET:
-        search = request.GET.get("search")
-        query_set = query_set.filter(tiffin_name__contains=search)
+    if request.method == 'POST':
+        post_data = request.POST.dict()
+        if post_data.get("avg_rating"):
+            post_data["avg_rating"] = float(post_data["avg_rating"])
+
+        if post_data.get("calories"):
+            calories = [int(c.strip()) for c in post_data["calories"].split("-")]
+            post_data["calories__gt"] = float(calories[0])
+            post_data["calories__lt"] = float(calories[1])
+            post_data.pop("calories")
+
+        if post_data.get('free_delivery_eligible'):
+            if post_data['free_delivery_eligible'] == "on":
+                post_data['free_delivery_eligible'] = True
+            else:
+                post_data['free_delivery_eligible'] = False
+
+        post_data.pop("csrfmiddlewaretoken")
+        tiffins = Tiffin.objects.filter(
+            **{k: v for k, v in post_data.items() if v != '' and v is not None})
     else:
-        query_set = query_set
+        if request.GET.get('search'):
+            tiffins = Tiffin.objects.filter(tiffin_name__contains=request.GET['search'])
+        else:
+            tiffins = Tiffin.objects.all()
 
-    print(filtersForm.is_valid())
-    print(filtersForm.changed_data)
+    formatted_tiffins = []
+    for tiffin in tiffins:
+        formatted_tiffins.append({"name": tiffin.tiffin_name, "photo": tiffin.image,
+                                  "business": tiffin.business_id.first_name + tiffin.business_id.last_name,
+                                  "rating": list(range(int(round(tiffin.avg_rating)))),
+                                  "price": tiffin.price, "id": tiffin.id})
+    search_form = ExploreSearchForm()
+    filters_form = FilterForm()
 
-    if filtersForm.is_valid() and filtersForm.changed_data:
-        mealType = filtersForm.cleaned_data.get('meal_type')
-        rating = filtersForm.cleaned_data.get('rating')
-        calorie_min = filtersForm.cleaned_data.get('calorie_range')
-        calorie_max = filtersForm.cleaned_data.get('calorie_range')
-        price_min = filtersForm.cleaned_data.get('price_range')
-        price_max = filtersForm.cleaned_data.get('price_range')
-        print(mealType, rating)
-        print(calorie_min, calorie_max, price_min, price_max)
+    return render(request, 'user_dashboard/explore.html',
+                  {'searchForm': search_form, 'filtersForm': filters_form,
+                   'tiffins': formatted_tiffins, "filter_params": {}})
 
-        if mealType:
-            query_set = query_set.filter(meal_type=mealType)
 
-        if rating:
-            query_set = query_set.filter(avg_rating__in=rating)
-    else:
-        query_set = query_set
-
-    for tiffin in query_set:
-        tiffins.append({"name": tiffin["tiffin_name"], "photo": tiffin["image"],
-                        "business": tiffin["business_id__first_name"] + tiffin["business_id__last_name"],
-                        "rating": list(range(int(round(tiffin["avg_rating"])))),
-                        "price": tiffin["price"]})
-
-    meal_types = Tiffin.objects.values_list("meal_type", flat=True).distinct()
-    meal_types = [MEAL[i] for i in meal_types]
-    return render(request, 'user_dashboard/explore.html', {'tiffins': tiffins,
-                                                           "meal_types": meal_types})
+def tiffindetails(request, tiffin_id):
+    return Tiffin.objects.get(id=tiffin_id)
