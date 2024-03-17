760c8b85f5e44221430aefb9aaf1c5421cb11070
Pratik Choudhari <40862682+pratik-choudhari@users.noreply.github.com>
2024-03-17 13:09:46 -0400

    Merge Explore into Main (#26)
    * Changes for filters
    
    * [FIX] use model forms for explore page filter
    
    * [FIX] integrate form ogic partially
    
    * [FIX] integrate search bar functionality
    
    * [FIX] integrate form logic partially
    
    * [FIX] improve aesthetics
    
    * Changes for filters
    
    * forms.py merge conflict resolved
    
    ---------
    
    Co-authored-by: Dhvani Sheth <dhvanisheth25@gmail.com>

---
+++ user_dashboard/forms.py
@@ -0,0 +1,52 @@
+from django.db.models import Max, Min
+
+from user_dashboard.models import Tiffin
+
+
+class ExploreSearchForm(forms.Form):
+    search = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'w-75',
+                                                                     'placeholder': 'Search tiffins or businesses'}))
+
+
+RATING_CHOICES = [
+    ('5', '⭐⭐⭐⭐⭐'),
+    ('4', '⭐⭐⭐⭐'),
+    ('3', '⭐⭐⭐'),
+    ('2', '⭐⭐'),
+    ('1', '⭐'),
+]
+
+
+def create_price_range():
+    min_ = float(Tiffin.objects.all().values_list('price', flat=True).annotate(Min('price')).order_by('price')[0])
+    max_ = float(Tiffin.objects.all().values_list('price', flat=True).annotate(Max('price')).order_by('-price')[0])
+    min_ = int(min_ - (min_ % 10))
+    max_ = int(max_ - (max_ % 10)) + 10
+    ranges = list(range(min_, max_, 10))
+    ranges = list(zip(ranges, ranges[1:]))
+    ranges = [(f"${r[0]} - ${r[1]}", f"${r[0]} - ${r[1]}") for r in ranges]
+    return ranges
+
+
+def create_calorie_range():
+    min_ = float(Tiffin.objects.all().values_list('calories', flat=True)
+                 .annotate(Min('calories')).order_by('calories')[0])
+    max_ = float(Tiffin.objects.all().values_list('calories', flat=True)
+                 .annotate(Max('calories')).order_by('-calories')[0])
+    min_ = int(min_ - (min_ % 100))
+    max_ = int(max_ - (max_ % 100)) + 100
+    ranges = list(range(min_, max_, 100))
+    ranges = list(zip(ranges, ranges[1:]))
+    ranges = [(f"{r[0]} - {r[1]}", f"{r[0]} - {r[1]}") for r in ranges]
+    return ranges
+
+
+class FilterForm(forms.ModelForm):
+    class Meta:
+        model = Tiffin
+        exclude = ["image", "tiffin_name", "tiffin_description", "schedule_id", "business_id"]
+        widgets = {"meal_type": forms.CheckboxSelectMultiple(choices=Tiffin.MEAL),
+                   "avg_rating": forms.CheckboxSelectMultiple(choices=RATING_CHOICES),
+                   "price": forms.RadioSelect(choices=create_price_range()),
+                   "calories": forms.RadioSelect(choices=create_calorie_range())}
+        labels = {"avg_rating": "Rating"}
--- user_dashboard/views.py
+++ user_dashboard/views.py
@@ -2,15 +2,44 @@ from django.shortcuts import render, get_object_or_404
 
 from .models import Tiffin, Review
 from enum_maps import MEAL
+from .forms import ExploreSearchForm, FilterForm
+
 
 
 def explore(request):
-    if request.method != 'GET':
-        return {"error": "Method Not Allowed"}
+    searchForm = ExploreSearchForm()
+    filtersForm = FilterForm()
     query_set = (Tiffin.objects.all()
                  .values("tiffin_name", "image", "business_id__first_name", "business_id__last_name", "avg_rating",
                          "price"))
     tiffins = []
+    if searchForm.is_valid() and 'query' in request.GET:
+        search = request.GET.get("search")
+        query_set = query_set.filter(tiffin_name__contains=search)
+    else:
+        query_set = query_set
+
+    print(filtersForm.is_valid())
+    print(filtersForm.changed_data)
+
+    if filtersForm.is_valid() and filtersForm.changed_data:
+        mealType = filtersForm.cleaned_data.get('meal_type')
+        rating = filtersForm.cleaned_data.get('rating')
+        calorie_min = filtersForm.cleaned_data.get('calorie_range')
+        calorie_max = filtersForm.cleaned_data.get('calorie_range')
+        price_min = filtersForm.cleaned_data.get('price_range')
+        price_max = filtersForm.cleaned_data.get('price_range')
+        print(mealType, rating)
+        print(calorie_min, calorie_max, price_min, price_max)
+
+        if mealType:
+            query_set = query_set.filter(meal_type=mealType)
+
+        if rating:
+            query_set = query_set.filter(avg_rating__in=rating)
+    else:
+        query_set = query_set
+
     for tiffin in query_set:
         tiffins.append({"name": tiffin["tiffin_name"], "photo": tiffin["image"],
                         "business": tiffin["business_id__first_name"] + tiffin["business_id__last_name"],
