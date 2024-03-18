f2c2596b8aad477e1792b51acf44efffe6c24973
Joel Mathew Koshy <joelmathewkoshy@gmail.com>
2024-03-18 15:46:04 -0400

    Business Sign up complete logic (#37)
    * Sign up forms and views - done
    
    * remove migration

--- business_dashboard/views.py
+++ business_dashboard/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from business_dashboard.forms import TiffinForm, SignUpForm
 from user_dashboard.models import Tiffin, TBUser
 from django.contrib.auth.forms import AuthenticationForm
@@ -13,15 +13,16 @@ def index(request):
 @login_required
 def tiffin(request):
     return render(request, template_name='business_dashboard/tiffin.html', context={})
+  
 @login_required
 def add_tiffin(request):
-    msg=''
+    msg = ''
     if request.method == 'POST':
         form = TiffinForm(request.POST)
         if form.is_valid():
             tiffin_item = form.save(commit=False)
             tiffin_item.save()
-            msg='Tiffin added'
+            msg = 'Tiffin added'
     else:
         form = TiffinForm()
     return render(request, 'business_dashboard/add_tiffin.html', context={'form': form, 'msg': msg})
@@ -33,7 +34,27 @@ def business_profile(request, username):
 
 
 def signup(request):
-    form = SignUpForm()
+    special_characters = '"!@#$%^&*()-+?=,<>/"'
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+
+        #verify the username
+        username = request.POST['username']
+        if any(char in special_characters for char in username):
+            form.add_error('username', 'Username cannot contain special characters')
+            return render(request, 'business_dashboard/sign-up.html', {'form': form})
+
+        if form.is_valid():
+            # validate the form data by not commiting into db
+            user = form.save()
+            user.set_password(form.cleaned_data.get('password1'))
+            user.client_type = 1
+            # save into db
+            user.save()
+
+            return redirect('business_dashboard:login')
+    else:
+        form = SignUpForm()
     return render(request, "business_dashboard/sign-up.html", {'form': form})
 
 def businessLoginPage(request):
