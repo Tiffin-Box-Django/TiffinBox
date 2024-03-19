6672b5626c5675137b09d2498824974139fda6bf
Joel Mathew Koshy <joelmathewkoshy@gmail.com>
2024-03-18 23:53:28 -0400

    profile image upload

--- TiffinBox/urls.py
+++ TiffinBox/urls.py
@@ -1,6 +1,8 @@
 from bootstrap_colors.views import BootstrapColorsView
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -9,4 +11,4 @@ urlpatterns = [
     path('business/', include("business_dashboard.urls")),
     # this is for overriding the bootstrap color variables
     path('colors.css', BootstrapColorsView.as_view(), name='colors')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
