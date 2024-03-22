e940c259f282808dfa3d677cb8266630964d0e2d
Dhvani Sheth <dhvanisheth25@gmail.com>
2024-03-21 23:09:36 -0400

    fixes for urls of profile and edit profile Added category for tiffins on explore added default user profile image

--- user_dashboard/urls.py
+++ user_dashboard/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('fake-data/', faker_view.insert_fake_data, name='fake-data'),
     path('explore/', views.explore, name='explore'),
     path('tiffin/<int:pk>/', views.TiffinDetails.as_view(), name='tiffindetails'),
-    path('addcart/<id>', views.addcart, name='add_to_cart'),
     path('signup/', views.signup, name='signup'),
     path('login/', views.UserLogin.as_view(), name='login'),
     path('logout/', views.logout, name='logout'),
@@ -18,6 +17,6 @@ urlpatterns = [
     path('update-cart/', views.update_cart, name='update-cart'),
     path('add-review/<int:tiffinid>', views.add_review, name='add-review'),
     path('activate/<uidb64>/<token>', views.activate, name='activate'),
-    path('profile/', views.user_profile, name='mainprofile'),
-    path('edit-profile/', views.edit_profile, name='profile'),
+    path('profile/', views.user_profile, name='profile'),
+    path('edit-profile/', views.edit_profile, name='editprofile'),
 ]
