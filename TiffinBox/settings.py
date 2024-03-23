497956dc231306a259361a76a079af8190941065
Dhvani Sheth <dhvanisheth25@gmail.com>
2024-03-23 17:46:05 -0400

    changes for order history page added generic tiffinbox email in settings Added logic for updating the total price of the order in update_cart

--- TiffinBox/settings.py
+++ TiffinBox/settings.py
@@ -147,8 +147,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'your_email'
-EMAIL_HOST_PASSWORD = 'ypur_password'
+EMAIL_HOST_USER = 'tiffinbox84@gmail.com'
+EMAIL_HOST_PASSWORD = 'yqxbqjarsdduueqn'
 
 PASSWORD_RESET_TIMEOUT = 14400
 
